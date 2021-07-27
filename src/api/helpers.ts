3fb49369e9d0caacd8462f4a455b70be46581677
John Li <johnzli1995@gmail.com>
2021-07-26 22:14:08 -0700

    Get a super colorful globe

---
+++ src/api/helpers.ts
@@ -0,0 +1,3 @@
+export function wrapUrlWithCorsProxy(url: string) {
+  return `https://cryptic-ocean-25473.herokuapp.com/${url}`;
+}
--- src/components/globe.tsx
+++ src/components/globe.tsx
@@ -6,7 +6,10 @@ import {useEffect} from 'react';
 import {useRef} from 'react';
 import {useState} from 'react';
 import {getDataUrl} from '../utils/import_utils';
+import classNames from 'classnames';
 import classes from './globe.less';
+import {getCountries} from '../api/joshuaproject';
+import _ from 'lodash';
 
 interface Props {
   onPolygonClick(): void;
@@ -21,10 +24,10 @@ enum Severity {
 }
 
 const ColorCodedSeverity = {
-  [Severity.Mild]: '#FFFFE1',
-  [Severity.Moderate]: '#F3758C',
-  [Severity.Severe]: '#7D2898',
-  [Severity.Extreme]: '#4C1E6E',
+  [Severity.Mild]: '#FFF2EB',
+  [Severity.Moderate]: '#FFCEC2',
+  [Severity.Severe]: '#E79C9F',
+  [Severity.Extreme]: '#AA747E',
 };
 
 // TODO (johnli): Look into hover tooltips
@@ -32,34 +35,86 @@ function Globe() {
   const globeRef = useRef<any>(null);
 
   const [countries, setCountries] = useState<Countries>({features: []});
+  const [countryData, setCountryData] = useState<Record<string, any>>({});
+  const [initialized, setInitialized] = useState<boolean> (false);
 
   const setupLighting = () => {
     const globe = globeRef.current;
-    if (globe) {
+    if (globe && !initialized) {
       const lighting = globe.scene();
       const ambientLighting = lighting.children[1];
       const directionalLighting = lighting.children[2];
   
-      ambientLighting.intensity = 1.05;
-      directionalLighting.intensity = 0.15;
+      ambientLighting.intensity = 1.25;
+      directionalLighting.intensity = 0.05;
+
+      setInitialized(true);
     }
   };
 
   const loadCountryData = async () => {
     const res = await fetch(getDataUrl('countries.geo.json'));
-    const data = await res.json();
-    setCountries(data);
+    const geoLocation = await res.json();
+
+    const {data: fetchedCountryData} = await getCountries();
+    const fetchedCountryDataMap = _.zipObject(
+      fetchedCountryData.data.map(country => country.ISO2),
+      fetchedCountryData.data,
+    );
+    
+
+    setCountries(geoLocation);
+    setCountryData(fetchedCountryDataMap);
   };
 
   // TODO (johnli): Have better coloring, pull information from Joshua Project.
-  const getPolygonColor = useCallback((_polygon) => {
-    return `#${Math.round(Math.random() * Math.pow(2, 24)).toString(16).padStart(6, '0')}`;
-  }, [countries]);
+  const getPolygonColor = useCallback((polygon) => {
+    let country = countryData[polygon.properties.iso_a2];
 
-  const globeMaterial = new THREE.MeshPhongMaterial();
-  globeMaterial.color = new THREE.Color(0xeaf2ff);
-  globeMaterial.shininess = 15;
-  globeMaterial.specular = new THREE.Color(0xeaf2ff);
+    if (polygon.properties.iso_a2 === '-99') {
+      if (polygon.properties.name === 'Somaliland') {
+        country = countryData['SO'];
+      }
+
+      if (polygon.properties.name === 'N. Cyprus') {
+        country = countryData['CY'];
+      }
+
+      if (polygon.properties.name === 'Kosovo') {
+        country = countryData['XK'];
+      }
+    }
+
+    const ratioOfUnreachedPeople = country.PoplPeoplesLR / country.PoplPeoples;
+    const numWorkersNeeded = country.WorkersNeeded ?? 0;
+
+    const reachScore = ratioOfUnreachedPeople * numWorkersNeeded;
+
+    // TODO (johnli): better coloring for defaults.
+    if (reachScore === 0) {
+      return '#FFFFE1';
+    }
+
+    if (reachScore < 10) {
+      return ColorCodedSeverity[Severity.Mild];
+    }
+
+    if (reachScore < 30) {
+      return ColorCodedSeverity[Severity.Moderate];
+    }
+
+    if (reachScore < 100) {
+      return ColorCodedSeverity[Severity.Severe];
+    }
+
+    return ColorCodedSeverity[Severity.Extreme];
+  }, [countryData]);
+
+  const globeMaterial = new THREE.MeshPhongMaterial({
+    shininess: 10,
+    color: new THREE.Color(0xEDFAFD),
+    specular: new THREE.Color(0xEDFAFD),
+  });
 
   useEffect(() => {
     void loadCountryData();
@@ -69,10 +124,10 @@ function Globe() {
   // Apparently does not work in useEffect.
   setupLighting();
   return (
-    <div className={classes.container}>
+    <div className={classNames(classes.container, {[classes.hide]: !initialized})}>
       <ReactGlobe
         ref={globeRef}
-        // globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+        waitForGlobeReady={true}
         globeMaterial={globeMaterial}
         polygonsData={countries.features.filter(d => d.properties.ISO_A2 !== 'AQ')}
         polygonLabel={({properties}: any /* Fix type later */) => {
@@ -80,7 +135,7 @@ function Globe() {
         }}
         polygonCapColor={getPolygonColor}
         backgroundColor={'rgba(0,0,0,0)'}
-        polygonStrokeColor={'#000000'}
+        polygonStrokeColor={'#FFFFFF'}
         showAtmosphere={false}
         height={1000}
         width={1000}
